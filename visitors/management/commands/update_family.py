e1b6f8064d89bd05eda087ecdf1fb8e0c48eac75
danielldt <49337080+danielldt@users.noreply.github.com>
2020-02-20 15:38:59 +0800

    Update update_family.py

--- visitors/management/commands/update_family.py
+++ visitors/management/commands/update_family.py
@@ -31,6 +31,8 @@ class Command(BaseCommand):
                     )
                     
                     data = {"data_type":"update","track_id":t.id, "status":t.status,"lot":0,"area":t.area.id,"resident":rlt.resident.user.id}
-                    con = get_redis_connection("default")
+                    #con = get_redis_connection("default")
+                    con = redis.StrictRedis(host='vmswebcache.redis.cache.windows.net',
+        port=6380, db=0, password='IsF++8DTZ01nNN3Ec5b5FS9xxoZYRD4Qs+UvG6FB5ew=', ssl=True)
                     con.setex("foo",10,str(data).replace('\'','"'))
 
